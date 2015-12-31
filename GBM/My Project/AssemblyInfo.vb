18686abced3019706c7dae0e349417959fae99e5
Michael J. Seiferling <mseiferling@gmail.com>
2015-12-30 21:20:07 -0600

    Reverted version for merge

--- GBM/My Project/AssemblyInfo.vb
+++ GBM/My Project/AssemblyInfo.vb
@@ -33,7 +33,7 @@ Imports System.Runtime.InteropServices
 ' by using the '*' as shown below:
 ' <Assembly: AssemblyVersion("1.0.*")> 
 
-<Assembly: AssemblyVersion("0.96.*")> 
-<Assembly: AssemblyFileVersion("0.96.0.0")> 
+<Assembly: AssemblyVersion("0.95.*")> 
+<Assembly: AssemblyFileVersion("0.95.0.0")> 
 
 <Assembly: NeutralResourcesLanguageAttribute("en")> 
