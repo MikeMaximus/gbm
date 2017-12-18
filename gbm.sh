35cb711ca162bf075717aae03971bdcb9a2c453d
Sebastian Riedel <git@basxto.de>
2017-12-18 21:14:41 +0100

    Remove BASHisms from start script

--- gbm.sh
+++ gbm.sh
@@ -1,23 +1,22 @@
 #!/bin/sh -e
 #check for all dependencies
-for prog in {mono,readlink,df,7za};do
+for prog in mono readlink df 7za;do
     [ -n "`whereis -b ${prog} | cut -sd' ' -f2`" ] || (echo "Please install ${prog}" && exit 1);
 done
 for lib in libsqlite3;do
     [ -n "`ldconfig -p | grep ${lib}`" ] || (echo "Please install ${lib}" && exit 1);
 done
-DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )";
+DIR="$( cd "$( dirname "$(pwd)/$0" )" && pwd )";
 echo "Located in ${DIR}";
 GBMPATH='./';
 #locate GBM.exe
-if [ \( "${DIR}" == '/bin' \) -a \( -s '/usr/share/gbm/GBM.exe' \) ]; then
+if [ "${DIR}" = '/bin' ] && [ -s '/usr/share/gbm/GBM.exe' ]; then
     GBMPATH='/usr/share/gbm/';
-elif [ \( "${DIR}" == '/usr/local/bin' \) -a \( -s '/usr/local/share/gbm/GBM.exe' \) ]; then
+elif [ "${DIR}" = '/usr/local/bin' ] && [ -s '/usr/local/share/gbm/GBM.exe' ]; then
     GBMPATH='/usr/local/share/gbm/';
 elif [ ! -s './GBM.exe' ]; then
     echo 'GBM.exe not found';
     exit 2;
 fi
 mono --desktop ${GBMPATH}'GBM.exe' "$@";
-exit 0;
-
+exit $?;
