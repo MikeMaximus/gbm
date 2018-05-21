539009c5de73433ed11f7be0d9dcd4fb11ed7838
Michael J. Seiferling <mseiferling@gmail.com>
2018-05-20 21:07:00 -0600

    Updated SQLite to 3.23.1

--- GBM/My Project/AssemblyInfo.vb
+++ GBM/My Project/AssemblyInfo.vb
@@ -33,7 +33,7 @@ Imports System.Runtime.InteropServices
 ' by using the '*' as shown below:
 ' <Assembly: AssemblyVersion("1.0.*")> 
 
-<Assembly: AssemblyVersion("1.1.1.*")>
-<Assembly: AssemblyFileVersion("1.1.1.0")>
+<Assembly: AssemblyVersion("1.1.2.*")>
+<Assembly: AssemblyFileVersion("1.1.2.0")>
 
 <Assembly: NeutralResourcesLanguageAttribute("en")> 
