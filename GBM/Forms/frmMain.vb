MikeMaximus/gbm

commit 2f778171bbd519de49de78385d0bc8ce1ae2531f

Hide buttons for monitor only games

GBM/Forms/frmMain.vb
@@ -2163,15 +2163,16 @@ Public Class frmMain
                     btnEdit.Visible = False
                     btnPlay.Visible = False
                 Else
-                    If oProcess.Duplicate Then
-                        btnRestore.Visible = False
-                        btnBackup.Visible = False
-                    Else
-                        btnRestore.Visible = True
-                        btnBackup.Visible = True
-                    End If
+                    btnRestore.Visible = False
+                    btnBackup.Visible = False
                     btnEdit.Visible = False
                     btnPlay.Visible = False
+                    If Not oProcess.Duplicate Then
+                        If Not oProcess.GameInfo.MonitorOnly Then
+                            btnRestore.Visible = True
+                            btnBackup.Visible = True
+                        End If
+                    End If
                 End If
                 lstGames.Enabled = False
                 txtSearch.Enabled = False

commit c9c527c30441692403dc429dc3969de7cfe97362

Fixed a possible crash caused by 2f778171

GBM/Forms/frmMain.vb
@@ -2170,8 +2170,9 @@ Public Class frmMain
                     btnBackup.Visible = False
                     btnEdit.Visible = False
                     btnPlay.Visible = False
-                    If Not oProcess.Duplicate Then
-                        If Not oProcess.GameInfo.MonitorOnly Then
+                    'Change these buttons only while currently monitoring a game
+                    If eCurrentStatus = eStatus.Monitoring Then
+                        If Not oProcess.Duplicate And Not oProcess.GameInfo.MonitorOnly Then
                             btnRestore.Enabled = True
                             btnRestore.Visible = True
                             btnBackup.Enabled = True